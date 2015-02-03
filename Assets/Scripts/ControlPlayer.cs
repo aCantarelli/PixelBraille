a1d4e58efddde74cfae85804582f567341c5cf39
aCantarelli <andrecantarelli@hotmail.com>
2015-02-02 22:52:23 -0200

    comeco programacao player

--- Assets/Scripts/ControlPlayer.cs
+++ Assets/Scripts/ControlPlayer.cs
@@ -3,14 +3,32 @@ using System.Collections;
 
 public class ControlPlayer : MonoBehaviour 
 {
-	
+	public Vector2 jumpForce;
+
 	void Start () 
 	{
-	
+		jumpForce = new Vector2(0f, 300f);
 	}
 
 	void Update () 
 	{
-	
+		if (Input.GetKeyDown (KeyCode.Space))
+						Attack ();
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+						Jump ();
+	}
+
+	void Jump()
+	{
+		//if(isGrounded)
+			print ("pula");
+			rigidbody2D.AddForce(new Vector2(0f, jumpForce.y));
+	}
+
+	void Attack()
+	{
+		//if(canAttack)
+			print ("ataca");
+			
 	}
 }
